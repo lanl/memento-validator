77569026cce7022e95cd90950505f7ac0fa2632a
Bhanuka Mahanama <mahanama94@gmail.com>
2021-08-26 17:51:36 -0400

    Finalized

--- mementoweb/validator/validator_types.py
+++ mementoweb/validator/validator_types.py
@@ -1,7 +1,34 @@
+#
+#  Copyright (c) 2021. Los Alamos National Laboratory (LANL).
+#  Written by: Bhanuka Mahanama (bhanuka@lanl.gov)
+#                     Research and Prototyping Team, SRO-RL,
+#                     Los Alamos National Laboratory
+#
+#  Correspondence: Lyudmila Balakireva, PhD (ludab@lanl.gov)
+#                     Research and Prototyping Team, SRO-RL,
+#                     Los Alamos National Laboratory
+#
+#  See LICENSE in the project root for license information.
+#
+
+#
+#
+#  Correspondence: Lyudmila Balakireva, PhD (ludab@lanl.gov)
+#                     Research and Prototyping Team, SRO-RL,
+#                     Los Alamos National Laboratory
+#
+#  See LICENSE in the project root for license information.
+#
+
 from enum import Enum
 
 
 class ResourceType(Enum):
+    """
+
+        Resource types. Used for typing support.
+
+    """
     ORIGINAL = "original"
 
     MEMENTO = "memento"
