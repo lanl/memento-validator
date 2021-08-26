43d4f2e7148b960fa8437be36d46eeec23b8c9f8
Bhanuka Mahanama <mahanama94@gmail.com>
2021-08-26 16:13:37 -0400

    Finalized

--- mementoweb/validator/errors/header_errors.py
+++ mementoweb/validator/errors/header_errors.py
@@ -1,3 +1,16 @@
+#
+#  Copyright (c) 2021. Los Alamos National Laboratory (LANL).
+#  Written by: Bhanuka Mahanama (bhanuka@lanl.gov)
+#                     Research and Prototyping Team, SRO-RL,
+#                     Los Alamos National Laboratory
+#
+#  Correspondence: Lyudmila Balakireva, PhD (ludab@lanl.gov)
+#                     Research and Prototyping Team, SRO-RL,
+#                     Los Alamos National Laboratory
+#
+#  See LICENSE in the project root for license information.
+#
+
 class HeadersNotFoundError(Exception):
     pass
 
