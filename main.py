ff6caf945ea50bd95cfed2e197c0b5cc17c87b30
Bhanuka Mahanama <mahanama94@gmail.com>
2021-08-27 15:11:15 -0400

    Added license headers.

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#
+#  Copyright (c) 2021. Los Alamos National Laboratory (LANL).
+#  Written by: Bhanuka Mahanama (bhanuka@lanl.gov)
+#                     Research and Prototyping Team, SRO-RL,
+#                     Los Alamos National Laboratory
+#
+#  Correspondence: Lyudmila Balakireva, PhD (ludab@lanl.gov)
+#                     Research and Prototyping Team, SRO-RL,
+#                     Los Alamos National Laboratory
+#
+#  See LICENSE in the project root for license information.
+#
+
 import click
 from pprint import pprint
 from mementoweb.apps.daily_validator.main import run as daily_validator_run
